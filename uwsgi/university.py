b1c7af35bb00e0c40d58175d258936d44a84c859
nat1998-k <77759698+nat1998-k@users.noreply.github.com>
2021-10-17 19:42:04 -0400

    added more code

--- uwsgi/university.py
+++ uwsgi/university.py
@@ -61,6 +61,53 @@ def showAllStudents(conn):
 
     return body
 
+
+def showStudentPage(conn, idNum):
+
+    body = """
+    <a href="./university.py">Return to main page.</a>
+    """
+
+    # get a cursor object. cursor object will help us run queries on the database
+    cursor = conn.cursor()
+
+    sql = """
+    SELECT *
+    FROM Student
+    WHERE id=%s
+    """
+    cursor.execute(sql, (int(idNum),))
+
+    # get the data from the database:
+    data = cursor.fetchall()
+
+    ## show student information
+    (idNum, os.name) = data[0]
+
+    body += """
+    <h2>%s %s's Student Page</h2>
+    <p>
+    <table border=1>
+        <tr>
+            <td>name</td>
+            <td>%s</td>
+        </tr>
+    </table>
+    """ % (
+        os.name,
+    )
+
+    ## provide an update button:
+    body += (
+        """
+    <FORM METHOD="POST" action="university.py">
+    <INPUT TYPE="HIDDEN" NAME="idNum" VALUE="%s">
+    <INPUT TYPE="SUBMIT" NAME="showUpdateStudentForm" VALUE="Update Profile">
+    </FORM>
+    """
+        % idNum
+    )
+
     ################################################################################
 def showAddStudentForm():
 
@@ -182,3 +229,79 @@ def deleteStudent(conn, idNum):
         return "Delete Student Succeeded."
     else:
         return "Delete Student Failed."
+
+def get_qs_post(env):
+    """
+    :param env: WSGI environment
+    :returns: A tuple (qs, post), containing the query string and post data,
+              respectively
+    """
+    # the environment variable CONTENT_LENGTH may be empty or missing
+    try:
+        request_body_size = int(env.get("CONTENT_LENGTH", 0))
+    except (ValueError):
+        request_body_size = 0
+    # When the method is POST the variable will be sent
+    # in the HTTP request body which is passed by the WSGI server
+    # in the file like wsgi.input environment variable.
+    request_body = env["wsgi.input"].read(request_body_size).decode("utf-8")
+    post = parse_qs(request_body)
+    return parse_qs(env["QUERY_STRING"]), post
+
+def application(env, start_response):
+    qs, post = get_qs_post(env)
+
+    body = ""
+    try:
+        conn = psycopg2.connect(
+            host="postgres",
+            dbname=os.environ["POSTGRES_DB"],
+            user=os.environ["POSTGRES_USER"],
+            password=os.environ["POSTGRES_PASSWORD"],
+        )
+    except psycopg2.Warning as e:
+        print(f"Database warning: {e}")
+        body += "Check logs for DB warning"
+    except psycopg2.Error as e:
+        print(f"Database error: {e}")
+        body += "Check logs for DB error"
+
+    idNum = None
+    if "idNum" in post:
+        idNum = post["idNum"][0]
+        ## handle case of starting to do an update -- show the form
+        if "showUpdateStudentForm" in post and "idNum" in post:
+            body += getUpdateStudentForm(conn, post["idNum"][0])
+        ## handle case of completing an update
+        elif "completeUpdate" in post:
+            body += updateStudent(
+                conn,
+                idNum,
+                post["name"][0],
+            )
+        ## handle case of showing a profile page
+        elif "deleteStudent" in post:
+            body += deleteStudent(conn, idNum)
+            idNum = None
+    ## handle case of adding a profile page:
+    elif "addStudent" in post:
+        b, idNum = addStudent(
+            conn,
+            post["name"][0],
+        )
+        body += b
+    elif "idNum" in qs:
+        idNum = qs.get("idNum")[0]
+    if idNum:
+        # Finish by showing the profile page
+        body += showStudentPage(conn, idNum)
+    # default case: show all profiles
+    else:
+        body += showAllStudents(conn)
+        body += showAddStudentForm()
+
+    start_response("200 OK", [("Content-Type", "text/html")])
+    return [wrapBody(body, title="University App").encode("utf-8")]
+
+if __name__ == "__main__":
+    main()
